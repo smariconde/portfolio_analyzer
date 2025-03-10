814fac976511b5fbff1d2a53affda437c43d8084
smariconde <64561537+smariconde@users.noreply.github.com>
2025-03-10 12:11:37 -0300

    Some fixes

--- sortino.py
+++ sortino.py
@@ -5,6 +5,14 @@ import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 import os
 
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'DNT': '1',  # Do Not Track Request Header
+        'Connection': 'close'
+    }
+
 def get_cedears():
     url = "https://www.comafi.com.ar/custodiaglobal/2483-Programas-Cedear.cedearnote.note.aspx#shares"
     tables = pd.read_html(url)
